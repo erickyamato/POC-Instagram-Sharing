cb17329ffc9c51fadd44998dac897ca66e931979
Erick Yamato <erick.yamato@me.com>
2023-04-12 11:56:07 -0300

    Initial Commit

---
+++ POC Instagram Sharing/POC_Instagram_SharingApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_Instagram_SharingApp.swift
+//  POC Instagram Sharing
+//
+//  Created by Erick Yamat on 12/04/23.
+//
+
+import SwiftUI
+
+@main
+struct POC_Instagram_SharingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
